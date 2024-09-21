c5e702d445ee488f0cdb702c61a12c1d6348f517
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-21 13:55:27 -0500

    first test added

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -2,3 +2,12 @@ require 'rspec'
 require './lib/activity'
 require './lib/reunion'
 
+RSpec.describe Reunion do
+    before(:each) do
+        @reunion = Activity.new("1406 BE")
+    end
+
+    it 'is an instance of reunion' do
+
+        expect(@reunion).to be_an_instance_of(Activity)
+    end
